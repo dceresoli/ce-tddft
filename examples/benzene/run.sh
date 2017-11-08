4a65f8121240bc61fb2d9dd2ec59ddfbc883542b
Davide Ceresoli <dceresoli@gmail.com>
2017-11-08 09:35:49 +0100

    Benzene example

---
+++ examples/benzene/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pw="mpirun -np 4 $HOME/Codes/qe-6.1/bin/pw.x"
+tddft="mpirun -np 4 $HOME/Codes/ce-tddft/bin/tddft.x"
+
+molecule=C6H6
+
+$pw <$molecule-scf.in > $molecule-scf.out
+
+for edir in x y z
+do
+  $tddft <$molecule-tddft_$edir.in > $molecule-tddft_$edir.out
+  grep ^DIP $molecule-tddft_$edir.out >dip_$edir.dat
+done
+
+
+
